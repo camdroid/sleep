2dc46c6a281812da4a9fd73147c91578cbf4ab16
Cam <camherringshaw@gmail.com>
2018-04-17 01:13:48 -0400

    Take timezone into account (sorta)

--- parse_spreadsheet.py
+++ parse_spreadsheet.py
@@ -1,4 +1,6 @@
 import csv
+from datetime import datetime
+import pytz
 
 with open('sleep_data.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -12,6 +14,12 @@ with open('sleep_data.csv') as csvfile:
             headers.append(row)
             count += 1
         elif count % 3 == 1:
+            timezone = pytz.timezone(row[1])
+            start_time = datetime.strptime(row[2], '%d. %m. %Y %H:%M')
+            end_time = datetime.strptime(row[3], '%d. %m. %Y %H:%M')
+            row[2] = timezone.localize(start_time)
+            row[3] = timezone.localize(end_time)
+            row.pop(1)
             data.append(row)
             count += 1
         elif not two_rows:
@@ -21,13 +29,21 @@ with open('sleep_data.csv') as csvfile:
             # We've gotten to the part of the spreadsheet where they switch to a 2-row formula
             two_rows = True
     actual_headers = headers[0][0:15]
+    actual_headers.pop(1)
     general_data = []
     general_data.append(actual_headers)
     for row in data:
-        general_data.append(row[0:15])
+        general_data.append(row[0:len(actual_headers)])
 
     with open('parsed_general_data.csv', 'w') as write_file:
+        tz = pytz.timezone('America/New_York')
         writer = csv.writer(write_file)
+        headers = True
         for row in general_data:
+            if not headers:
+                for i in [1, 2]:
+                    row[i] = row[i].astimezone(tz=tz).strftime('%Y/%m/%d %H:%M')
+            if headers:
+                headers = False
             writer.writerow(row)
 
